e4fe6602a68912a6657962d3fc3747b0cf912efc
jnpx <jnpx@users.noreply.github.com>
2016-07-15 12:36:58 +0200

    new theme code

---
+++ sites/all/themes/sig/js/block1.js
@@ -0,0 +1,87 @@
+/**
+ * @file
+ * Block block 1 animation
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+//Drupal.behaviors.sigBlock1Animation = {
+//  attach: function(context, settings) {
+    //Function only needs to be called on scroll
+    $(window).scroll(function() {
+      //Only needs to be called when the svg is available
+      if ($('#city').length) {
+
+          //Variables
+        $element = $('svg');
+        $airplane = $('#airplane')
+        $bus = $('#bus')
+        $clouds = $('.st0')
+        $win = $(window);
+
+        //Different scroll effects for smaller screens
+        if ($(window).width() <=800) {
+          $devider = 0.5;
+        }
+        else{
+          $devider = 2.5;
+        }
+        //Variables for the width of the element, distance to top,...
+        var viewportHeight = $(window).height(),
+          scrollTop = $win.scrollTop(),
+          elementOffsetTop = $element.offset().top,
+          elementHeight = $element.height();
+          elementWidth = $element.width()/$devider;
+        //Calculation for when the elemnt is in the vieuwport
+        var distance = (scrollTop + viewportHeight) - elementOffsetTop -elementWidth;
+        
+        // Scroll effect for the plane
+            if ($airplane) {
+              //Variable for the movement change
+            var translate = distance +'px,'+-distance/6+ 'px';
+              $airplane.css({
+                //Different transforms for all browser support
+                  '-webkit-transform': 'translate(' + translate + ')',
+                  '-moz-transform': 'translate(' + translate + ')',
+                  '-ms-transform': 'translate(' + translate + ')',
+                  '-o-transform': 'translate(' + translate + ')',
+                  'transform': 'translate(' + translate + ')'
+              });
+            }
+        // Scroll effect for the bus
+            if ($bus) {
+            var translate = distance/3 +'px, -0px';
+              $bus.css({
+                  '-webkit-transform': 'translate(' + translate + ')',
+                  '-moz-transform': 'translate(' + translate + ')',
+                  '-ms-transform': 'translate(' + translate + ')',
+                  '-o-transform': 'translate(' + translate + ')',
+                  'transform': 'translate(' + translate + ')'
+              });
+            }
+        // Scroll effect for the clouds
+            if ($clouds) {
+              var translate = (distance/10)*-1 +'px, -0px';
+              $clouds.css({
+                  '-webkit-transform': 'translate(' + translate + ')',
+                  '-moz-transform': 'translate(' + translate + ')',
+                  '-ms-transform': 'translate(' + translate + ')',
+                  '-o-transform': 'translate(' + translate + ')',
+                  'transform': 'translate(' + translate + ')'
+              });
+            }
+      }   
+    });
+    //*****************************
+    // Scroll END
+    //*****************************
+//  }
+//};
+
+
+})(jQuery, Drupal, this, this.document);
---
+++ sites/all/themes/sig/templates/field--field-slogan-image.tpl.php
@@ -0,0 +1,1 @@
+<div class="slogan-image-bg" style="background-image: url(<?php foreach ($items as $delta => $item): ?><?php print render($item); ?><?php endforeach; ?>)"></div>
